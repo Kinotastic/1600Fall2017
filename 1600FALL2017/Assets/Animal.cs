e4aa9c3d3dfcc59c8b5c7749dbaa1ca0d1be578c
Kino <10818333@my.uvu.edu>
2017-08-30 11:37:44 -0600

    Added more code

---
+++ 1600FALL2017/Assets/Animal.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animal : MonoBehaviour {
+
+	// Use this for initialization
+	public virtual void Start () {
+		Die();
+		Eat();
+		Sleep();
+	}
+	
+	void Die () {
+		print(this.name + " Dies");
+	}
+
+	void Eat () {
+		print(this.name + " Eats");
+	}
+
+	void Sleep () {
+		print(this.name + " Sleeps");
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
