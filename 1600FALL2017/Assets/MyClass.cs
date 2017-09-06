36bcc6a170240b8c07ec96ab5a12e4a2c7d1b308
Kino <10818333@my.uvu.edu>
2017-09-06 11:48:10 -0600

    Learned parameters and strings

---
+++ 1600FALL2017/Assets/MyClass.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyClass : MonoBehaviour {
+
+	string myString = "Hello";
+
+	// Parameters are holders/containers
+	// "=" denotes an assignment
+	// Use this for initialization
+	void Start () {
+		print(myString);
+	}
+	// Everything you open you must close
+
+}
