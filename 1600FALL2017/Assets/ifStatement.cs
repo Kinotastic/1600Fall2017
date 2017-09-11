3be61ee9f7351afb4d0430cda460ab9542a75942
Kino <10818333@my.uvu.edu>
2017-09-11 11:39:22 -0600

    Added ifStatement

---
+++ 1600FALL2017/Assets/ifStatement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ifStatement : MonoBehaviour {
+
+	public int num1;
+	public int num2;
+	public int value;
+
+	public bool canPlay = true;
+
+	public string input;
+	private string password = "password";
+
+	void Start () {
+		if(num1 + num2 == value)
+		{
+			print(value);
+		}
+
+		if (canPlay)
+		{
+			print("Play");
+		}
+
+		if (input == password)
+		{
+			print("You know the password");
+		}
+	}
+}
