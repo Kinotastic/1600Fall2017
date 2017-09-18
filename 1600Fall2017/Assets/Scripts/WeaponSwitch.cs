6395ef420986f83b0d11b77c97acb4386167d080
Kino <10818333@my.uvu.edu>
2017-09-18 11:33:27 -0600

    Added weapon switches

---
+++ 1600Fall2017/Assets/Scripts/WeaponSwitch.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponSwitch : MonoBehaviour {
+
+	public string[] weapons;
+	
+	public void Start (){
+		switch (weapons[0])
+		{
+			case "Missile":
+				print("Missile");
+				break;
+
+			case "Plasma":
+				print("Plasma");
+				break;
+		}
+	}
+}
