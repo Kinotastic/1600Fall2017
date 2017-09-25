e19220667295e663720ad1769cc24453178fdded
Kino <10818333@my.uvu.edu>
2017-09-25 11:29:53 -0600

    Added lists.cs

---
+++ 1600FALL2017/Assets/Scripts/Lists.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Lists : MonoBehaviour 
+
+{
+	public string[] weapons = {"Gun", "Knife", "Rifle"};	
+}
--- 1600Fall2017/Assets/Scripts/Loops.cs
+++ 1600Fall2017/Assets/Scripts/Loops.cs
@@ -3,20 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Loops : MonoBehaviour {
-
 	public bool canPlay = true;
-
 	void Start() {
 		StartCoroutine(PlayGame());
 	}
-
 	IEnumerator PlayGame () {
-
 		while (canPlay) {
 			print("Playing");
 			yield return new WaitForSeconds(0.1f);
 		}
 		print("End Game");
 	}
-
 }
