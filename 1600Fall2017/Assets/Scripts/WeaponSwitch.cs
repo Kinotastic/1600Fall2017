3aa629686ad36ed69925f6236b876fb351259c26
Kino <10818333@my.uvu.edu>
2017-09-30 11:56:11 -0600

    Added syntax comments

--- 1600Fall2017/Assets/Scripts/WeaponSwitch.cs
+++ 1600Fall2017/Assets/Scripts/WeaponSwitch.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//Syntax is the grammar of scripting
 public class WeaponSwitch : MonoBehaviour {
 
 	public string[] weapons;
+	//Each line must end with a semicolon
+	//Open brackets must always be closed
 	
 	public void MyWeaponSwitch (int i){
 		switch (weapons[i])
@@ -12,6 +15,7 @@ public class WeaponSwitch : MonoBehaviour {
 			case "Missile":
 				print("Missile");
 				break;
+				//Identifiers must always be capitalized
 
 			case "Plasma":
 				print("Plasma");
