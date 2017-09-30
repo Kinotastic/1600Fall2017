fa197d5b66ef22a721e022575c3c8880a8bd71d9
Kino <10818333@my.uvu.edu>
2017-09-30 15:08:10 -0600

    Added var comments

--- 1600Fall2017/Assets/Vars.cs
+++ 1600Fall2017/Assets/Vars.cs
@@ -1,8 +1,39 @@
 ﻿using UnityEngine;
 public class Vars : MonoBehaviour {
-	public float health = 100;
+	public float health = 100; //this is a variable.
 	void OnTriggerEnter()
 	{
-		health += 20;
+		health += 20; //this statement adds a number to the original variable.
 	}
 }
+
+/* Some more examples of vars:
+public class Variables : MonoBehaviour
+{
+	int myInt = 5 //the 5 is a variable in this statement
+	void Start (){
+		myInt = MultiplyByTwo(myInt);
+		Debug.Log (myInt);
+			}
+	int MultiplyByTwo(int number){
+		int ret;
+		ret = number * 2
+		return net; //the console will print the result
+					//that result will be 10
+
+	
+Additional examples:
+
+int MyInt = 3
+void Start (){
+	myInt = AddTwo(myInt);
+	Debug.Log (myInt);
+		}
+int AddTwo(int number){
+	int ret;
+	ret = number + 2
+	return net; //the result is 5
+}
+	}
+}
+*/
